21b804d0fe35c573f2e48367c30561316ca513cd
VneTraffiqua <safe72again@gmail.com>
2022-08-13 02:40:21 -0700

    Create datacenter/storage_information_view.py

---
+++ datacenter/storage_information_view.py
@@ -0,0 +1,23 @@
+from django.utils import timezone
+
+from datacenter.models import Visit
+from django.shortcuts import render
+
+
+
+def storage_information_view(request):
+    # Программируем здесь
+    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    all_non_closed_visits = []
+    for visit in non_closed_visits:
+        visit_info = {
+            'who_entered': visit.passcard,
+            'entered_at': timezone.localtime(visit.entered_at),
+            'duration': Visit.format_duration(visit.get_duration()),
+            'is_strange': Visit.is_visit_long(visit),
+        }
+        all_non_closed_visits.append(visit_info)
+    context = {
+        'non_closed_visits': all_non_closed_visits,
+    }
+    return render(request, 'storage_information.html', context)
