f89a40f61b419c8babc70c81050898a87ec8eff5
VneTraffiqua <safe72again@gmail.com>
2022-08-13 02:39:53 -0700

    Update datacenter/models.py

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import django.utils.timezone
+import datetime
 
 
 class Passcard(models.Model):
@@ -22,9 +24,29 @@ class Visit(models.Model):
     def __str__(self):
         return '{user} entered at {entered} {leaved}'.format(
             user=self.passcard.owner_name,
-            entered=self.entered_at,
+            entered=django.utils.timezone.localtime(
+                value=self.entered_at
+            ),
             leaved=(
                 f'leaved at {self.leaved_at}'
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_duration(visit):
+        """рассчитывает длительность визита"""
+        now_time = datetime.datetime.now(datetime.timezone.utc)
+        if visit.leaved_at:
+            difference_in_time = visit.leaved_at - visit.entered_at
+        else:
+            difference_in_time = now_time - visit.entered_at
+        return difference_in_time
+
+    def format_duration(duration):
+        visit_time_in_sec = int(datetime.timedelta.total_seconds(duration))
+        return f'{visit_time_in_sec // 3600}:{(visit_time_in_sec // 60) % 60}'
+
+    def is_visit_long(visit, minutes=60):
+        visit_time = Visit.get_duration(visit)
+        visit_time_in_sec = int(datetime.timedelta.total_seconds(visit_time))
+        return (visit_time_in_sec // minutes * 60) >= minutes * 60
