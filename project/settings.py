ea1f2889115ea540b59a6775c01c5b0603f10cde
VneTraffiqua <safe72again@gmail.com>
2022-08-13 02:41:45 -0700

    Create project/settings.py

---
+++ project/settings.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv(dotenv_path='./.env')
+db_settings = os.getenv('DB_SETTINGS')
+
+DATABASES = {
+    'default': dj_database_url.parse(db_settings)
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+DEBUG = os.getenv('DEBUG')
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
