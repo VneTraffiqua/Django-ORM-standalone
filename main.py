2d2267fa9c56048110c35c42f3daf0f450d59089
VneTraffiqua <safe72again@gmail.com>
2022-08-13 02:41:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
+from django.core.management import execute_from_command_line
 
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-django.setup()
-
-from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
