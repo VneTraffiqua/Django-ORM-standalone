1070dc186ed487ccaa520374d5e42316f10c17b7
VneTraffiqua <safe72again@gmail.com>
2022-08-25 14:04:11 -0700

    Update passcard_info_view

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,22 +2,23 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from . import helper_script
+
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
     all_visits = []
     for visit in visits:
-        duration = Visit.get_duration(visit)
-        visit_duration = Visit.format_duration(duration)
+        duration = visit.get_duration()
+        visit_duration = helper_script.format_duration(duration)
 
         this_passcard_visits = {
                 'entered_at': timezone.localtime(visit.entered_at),
                 'duration': visit_duration,
-                'is_strange': Visit.is_visit_long(visit)
+                'is_strange': visit.is_visit_long()
             }
-        all_visits.append(this_passcard_visits)
-    all_visits.reverse()
+        all_visits.insert(0, this_passcard_visits)
     context = {
         'passcard': passcard,
         'this_passcard_visits': all_visits
