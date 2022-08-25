c85c2466397467277452f6538f4f170a883f3473
VneTraffiqua <safe72again@gmail.com>
2022-08-25 14:03:43 -0700

    Update models.py

--- datacenter/models.py
+++ datacenter/models.py
@@ -33,20 +33,15 @@ class Visit(models.Model):
             )
         )
 
-    def get_duration(visit):
-        """рассчитывает длительность визита"""
+    def get_duration(self):
         now_time = datetime.datetime.now(datetime.timezone.utc)
-        if visit.leaved_at:
-            difference_in_time = visit.leaved_at - visit.entered_at
+        if self.leaved_at:
+            difference_in_time = self.leaved_at - self.entered_at
         else:
-            difference_in_time = now_time - visit.entered_at
+            difference_in_time = now_time - self.entered_at
         return difference_in_time
 
-    def format_duration(duration):
-        visit_time_in_sec = int(datetime.timedelta.total_seconds(duration))
-        return f'{visit_time_in_sec // 3600}:{(visit_time_in_sec // 60) % 60}'
-
-    def is_visit_long(visit, minutes=60):
-        visit_time = Visit.get_duration(visit)
+    def is_visit_long(self, minutes=60):
+        visit_time = self.get_duration()
         visit_time_in_sec = int(datetime.timedelta.total_seconds(visit_time))
         return (visit_time_in_sec // minutes * 60) >= minutes * 60
